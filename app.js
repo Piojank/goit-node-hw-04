f385e74964ff283a37cc8027503daf92eeea374d
piojank <p.jankowski931026@gmail.com>
2023-08-28 19:42:43 +0200

    initialize repository

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const contactsRouter = require("./api/contacts");
+
+app.use("/api/contacts", contactsRouter);
+
+app.use((_, res, __) => {
+    res.status(404).json({
+        status: "error",
+        code: 404,
+        message: "Use api on routes: /api/",
+        data: "Not found",
+    });
+});
+
+app.use((err, _, res, __) => {
+    console.log(err.stack);
+    res.status(500).json({
+        status: "fail",
+        code: 500,
+        message: err.message,
+        data: "Internal Server Error",
+    });
+});
+
+module.exports = app;
